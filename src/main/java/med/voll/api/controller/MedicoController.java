6f54fbe127e7ab68de9cbccd33ac324280453536
Elem Viviane <131198461+ElemViviane@users.noreply.github.com>
2023-08-08 07:47:00 -0300

    Criando

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,37 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados){
+        repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosCadastroMedicos dados){
+
+    }
+
+}
---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,54 @@
+package med.voll.api.endereco;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String numero;
+    private String complemento;
+
+    public Endereco(DadosEndereco dados) {
+        this.bairro = dados.bairro();
+        this.logradouro = dados.logradouro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
+
+    public void atualizarEndereco(DadosEndereco dados) {
+        if(dados.bairro() != null){
+            this.bairro = dados.bairro();
+        }
+        if(dados.logradouro() != null){
+            this.logradouro = dados.logradouro();
+        }
+        if(dados.cep() != null){
+            this.cep = dados.cep();
+        }
+        if(dados.cidade() != null){
+            this.cidade = dados.cidade();
+        }
+        if(dados.uf() != null){
+            this.uf = dados.uf();
+        }
+        if(dados.numero() != null){
+            this.numero = dados.numero();
+        }
+        if(dados.complemento() != null){
+            this.complemento = dados.complemento();
+        }
+    }
+}
